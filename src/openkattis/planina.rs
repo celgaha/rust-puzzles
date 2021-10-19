3f9ccbc264681e9e6f959b07c989abf9c263d60a
celgaha <78968916+celgaha@users.noreply.github.com>
2021-10-19 17:07:42 +0200

    Kattis Planina

--- src/openkattis/planina.rs
+++ src/openkattis/planina.rs
@@ -3,7 +3,7 @@ use crate::util::read::readone;
 #[allow(dead_code)]
 pub fn main() {
     let mut s = String::new();
-    let a = readone::<i32>(&mut s);
-    let b = (2 ^ a + 1) ^ 2;
+    let a = readone::<u32>(&mut s);
+    let b = (2i32.pow(a) + 1).pow(2);
     println!("{}", b)
 }
