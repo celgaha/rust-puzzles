0fbdfdedc06157a53ef3bb3fe5f4cb35d3623f2b
Anton Golov <jesyspa@gmail.com>
2021-10-25 18:07:47 +0200

    Move sort around (magictrick)

--- src/openkattis/magictrick.rs
+++ src/openkattis/magictrick.rs
@@ -1,8 +1,9 @@
-use puzzles::read::readvec;
+use puzzles::read::readone;
 
 fn main() {
     let mut s = String::new();
-    let v = readvec::<char>(&mut s).sort();
+    let mut v = readone::<String>(&mut s).chars().collect::<Vec<_>>();
+    v.sort();
     let mut outcome = 1;
     for k in 0..v.len() - 1 {
         if v[k] == v[k + 1] {
