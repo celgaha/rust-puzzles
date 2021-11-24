bbcb1204c854851b55acee87c5633f67284c1e3f
celgaha <78968916+celgaha@users.noreply.github.com>
2021-11-24 21:37:51 +0100

    bela

--- src/openkattis/bela.rs
+++ src/openkattis/bela.rs
@@ -2,12 +2,12 @@ use puzzles::read::{readone, readvec};
 
 fn main() {
     let mut s = String::new();
-    let v = readvec::<char>(&mut s);
-    let dominant = v[1];
+    let v = readvec::<String>(&mut s);
+    let dominant = &v[1];
     let mut points = 0;
-    for _ in 0..4 * v[0] as i32 {
+    for _ in 0..4 * v[0].parse::<usize>().unwrap() {
         let mut card = readone::<String>(&mut s);
-        let suit = card.pop();
+        let suit = card.pop().unwrap();
         if card == "A" {
             points += 11;
         } else if card == "K" {
@@ -16,11 +16,11 @@ fn main() {
             points += 3;
         } else if card == "T" {
             points += 10;
-        } else if card == "J" && suit == dominant {
+        } else if card == "J" && dominant.starts_with(suit) {
             points += 20;
         } else if card == "J" {
             points += 2;
-        } else if card == "9" && suit == dominant {
+        } else if card == "9" && dominant.starts_with(suit) {
             points += 14;
         }
     }
