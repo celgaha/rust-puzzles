a7e1e95a00202445cc334c6e17ce64c3a93a6866
celgaha <78968916+celgaha@users.noreply.github.com>
2021-11-27 21:51:54 +0100

    Friso helped

---
+++ src/openkattis/filip.rs
@@ -0,0 +1,13 @@
+use puzzles::read::readvec;
+
+fn main() {
+    let numbers = readvec::<String>(&mut String::new());
+    let revone = numbers[0].chars().rev().collect::<String>().parse::<i32>().unwrap();
+    let revtwo = numbers[1].chars().rev().collect::<String>().parse::<i32>().unwrap();
+    if revone > revtwo {
+        println!("{}", revone);
+    }
+    else {
+        println!("{}", revtwo);
+    }
+}
--- src/openkattis/trik.rs
+++ src/openkattis/trik.rs
@@ -4,13 +4,13 @@ fn main() {
     let moves = readone::<String>(&mut String::new());
     let mut ball = 1;
     for k in moves.chars() {
-        if &k == "A" {
+        if k == 'A' {
             if ball == 1 {
                 ball = 2;
             } else if ball == 2 {
                 ball = 1;
             }
-        } else if k == "B" {
+        } else if k == 'B' {
             if ball == 2 {
                 ball = 3;
             } else if ball == 3 {
