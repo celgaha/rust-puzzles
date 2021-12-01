c63b67af7a25f6dcad70f69a3a9e853835136bbf
celgaha <78968916+celgaha@users.noreply.github.com>
2021-12-01 16:30:45 +0100

    Day 1 AoC

--- src/aoc2021/day1-1.rs
+++ src/aoc2021/day1-1.rs
@@ -4,12 +4,12 @@ fn main() {
     let mut s = String::new();
     let mut result = 0;
     let mut last = 0;
-    for _ in 0..5 {
+    for _ in 0..2000 {
         let depth = readone::<i32>(&mut s);
         if depth > last {
             result += 1;
         }
         last = depth;
     }
-    println!("{}", result);
+    println!("{}", result - 1);
 }
